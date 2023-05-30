b636b5bc0d0266e5207bea808d592bc344638634
Erich Gubler <erichdongubler@gmail.com>
2023-05-30 10:45:20 -0400

    docs: add `FIXME` for local branch failure

--- src/main.rs
+++ src/main.rs
@@ -123,6 +123,7 @@ fn main() {
                 let mut branches = branches(config, &|cmd| cmd.arg(base))?;
                 branches.push("HEAD".to_owned());
                 if !head_is_detached()? {
+                    // FIXME: local branch with no upstream still fails. :frown:
                     branches.push("HEAD@{u}".to_owned());
                 }
                 branches

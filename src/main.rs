24f825602013dcde76c1984613bb60bd6cca75f6
Erich Gubler <erichdongubler@gmail.com>
2023-07-13 18:27:13 -0400

    docs: unify concise tagline b/w binary `--help` and `Cargo.toml`

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use clap::Parser;
 use ezcmd::EasyCommand;
 use git_glimpse::{git_config, list_branches_cmd, run, show_graph, stdout_lines};
 
-/// Show a minimal graph of Git commits.
+/// Show a minimal graph of Git commits for various use cases.
 ///
 /// When no arguments are specified, this commands runs as if the `stack` subcommand was invoked
 /// with no arguments.
